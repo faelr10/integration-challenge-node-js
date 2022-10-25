ac1121aaecc390268af9b861f1b262c76d25a72f
Rafael Victor <rafaelv.boscato@hotmail.com>
2022-10-25 20:28:01 -0300

    ✨ add module integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { IntegrationModule } from './integration/integration.module';
 
 @Module({
-  imports: [],
+  imports: [IntegrationModule],
   controllers: [AppController],
   providers: [AppService],
 })
