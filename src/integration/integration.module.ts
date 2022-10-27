7244edfd23751453342e88312982cc5017249fb8
Rafael Victor <rafaelv.boscato@hotmail.com>
2022-10-27 11:36:43 -0300

    ⚰️

--- src/integration/integration.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { IntegrationController } from './integration.controller';
-
-@Module({
-  controllers: [IntegrationController],
-  providers: [],
-})
-export class IntegrationModule {}
